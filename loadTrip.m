9177e78c6b15f5859cc8bbd2617c6b7a48bf3233
Richard <richardmason2010@gmail.com>
2015-02-06 09:55:51 +0000

    modified loadTrip.m

--- loadTrip.m
+++ loadTrip.m
@@ -3,6 +3,6 @@ function M = loadTrip(driver,trip)
 %takes in two numbers, driver and trip that identifies which
 %trip by which driver to load up into an array.
 
-filename= strcat('./',num2str(driver),'/',num2str(trip),'.csv');
+filename= strcat('../DriverData/',num2str(driver),'/',num2str(trip),'.csv');
 
 M = csvread(filename,1,0);
